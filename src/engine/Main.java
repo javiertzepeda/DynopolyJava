b4d3e0d52b05e46e10568c357b5f45a1ead7b6cf
javiertzepeda <javier.t.zepeda@utexas.edu>
2017-11-16 00:12:45 -0600

    Added title sequence and changed engine file layout.

--- src/engine/Main.java
+++ src/engine/Main.java
@@ -10,7 +10,7 @@ Author: Javier T Zepeda
 Date: 10/19/2016
 */
 
-public class Engine{
+public class Main{
 	//#include "MonopolyLibrary.h"
 	
 	
@@ -226,6 +226,42 @@ public class Engine{
 	}
 	
 	public static void main(String[] args) {
+		//dynopoly ascii wording developed using http://patorjk.com/software/taag/#p=display&f=Mer&t=Dynopoly!%20
+		//world ascii art found at http://www.chris.com/ascii/
+		System.out.println("             __gggrgM**M#mggg__");
+		System.out.println("         __wgNN@\"B*P\"\"mp\"\"@d#\"@N#Nw__");
+		System.out.println("       _g#@0F_a*F#  _*F9m_ ,F9*__9NG#g_");
+		System.out.println("    _mN#F  aM\"    #p\"    !q@    9NL \"9#Qu_");
+		System.out.println("   g#MF _pP\"L  _g@\"9L_  _g\"\"#__  g\"9w_ 0N#p");
+		System.out.println(" _0F jL*\"   7_wF     #_gF     9gjF   \"bJ  9h_");
+		System.out.println("j#  gAF    _@NL     _g@#_      J@u_    2#_  #_");
+		System.out.println(",FF_#\" 9_ _#\"  \"b_  g@   \"hg  _#\"  !q_ jF \"*_09_");
+		System.out.println("F N\"    #p\"      Ng@       `#g\"      \"w@    \"# t");
+		System.out.println("j p#    g\"9_     g@\"9_      gP\"#_     gF\"q    Pb L");
+		System.out.println("0J  k _@   9g_ j#\"   \"b_  j#\"   \"b_ _d\"   q_ g  ##");
+		System.out.println("#F  `NF     \"#g\"       \"Md\"       5N#      9W\"  j#");
+		System.out.println("  _____                                _       _  ");
+		System.out.println(" |  __ \\                              | |     | | ");
+		System.out.println(" | |  | |_   _ _ __   ___  _ __   ___ | |_   _| |");
+		System.out.println(" | |  | | | | | '_ \\ / _ \\| '_ \\ / _ \\| | | | | | ");
+		System.out.println(" | |__| | |_| | | | | (_) | |_) | (_) | | |_| |_| ");
+		System.out.println(" |_____/ \\__, |_| |_|\\___/| .__/ \\___/|_|\\__, (_) ");
+		System.out.println("          __/ |           | |             __/ | ");
+		System.out.println("         |___/            |_|            |___/   ");
+		
+		System.out.println("#k  jFb_    g@\"q_     _*\"9m_     _*\"R_    _#Np  J#");
+		System.out.println("tApjF  9g  J\"   9M_ _m\"    9%_ _*\"   \"#  gF  9_jNF");
+		System.out.println("k`N    \"q#       9g@        #gF       ##\"    #\"j");
+		System.out.println("`_0q_   #\"q_    _&\"9p_    _g\"`L_    _*\"#   jAF,'");
+		System.out.println("9# \"b_j   \"b_ g\"    *g _gF    9_ g#\"  \"L_*\"qNF");
+		System.out.println(" \"b_ \"#_    \"NL      _B#      _I@     j#\" _#\"");
+		System.out.println("   NM_0\"*g_ j\"\"9u_  gP  q_  _w@ ]_ _g*\"F_g@");
+		System.out.println("    \"NNh_ !w#_   9#g\"    \"m*\"   _#*\" _dN@\"");
+		System.out.println("       9##g_0@q__ #\"4_  j*\"k __*NF_g#@P\"");
+		System.out.println("         \"9NN#gIPNL_ \"b@\" _2M\"Lg#N@F\"");
+		System.out.println("             \"\"P@*NN#gEZgNN@#@P\"\"");
+
+		
 		/* prompts user for number of players in the game */
 		System.out.println("How many players? ");
 		Scanner reader = new Scanner(System.in);
